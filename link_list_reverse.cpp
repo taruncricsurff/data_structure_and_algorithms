e4f0cdd1dca7bf6c6f129098be5e5a268a43cb38
tarun_prakash <t-tarun.prakash@ocltp.com>
2019-03-04 16:44:29 +0530

    program_2_link_list_reverse

---
+++ link_list_reverse.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+struct node {
+
+	int value;
+	node * next;
+
+};
+
+node* rev(node* head) {
+
+
+	if(head->next == NULL) {
+
+		return head;
+
+	} else {
+
+		node* temp = head;
+
+		rev(head->next)->next = temp;
+		
+		temp->next = NULL;
+
+		return temp;
+
+	}
+
+}
+
+
+int main() {
+
+
+	struct node* head = new node;
+
+	head->value = 1;
+
+
+	head->next = new node;
+	head->next->value = 2;
+
+
+	head->next->next = new node;
+	head->next->next->value = 3;
+
+
+	head->next->next->next = new node;
+	head->next->next->next->value = 4;
+
+
+	head->next->next->next->next = NULL;
+
+
+
+
+
+
+	node* m =  head;
+
+	node*n;
+
+	cout<<"original list\n";
+	
+	while(head != NULL) {
+
+				cout<<head<<"\n";
+if(head->next == NULL)
+			n = head;
+		head = head->next;
+		
+
+	}
+
+
+head=m;
+
+cout<<"new head of link list"<<n<<"\n";;
+rev(m);
+
+
+	while(n != NULL) {
+
+		cout<<n<<"\n";
+
+		n = n->next;
+	}
+
+
+	return 0;
+
+
+}
+
+
+
+
+
