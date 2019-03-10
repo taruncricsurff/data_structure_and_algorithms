e5297ef8a51b0194418154903f13629e8adae886
tarun_prakash <t-tarun.prakash@ocltp.com>
2019-03-11 01:34:11 +0530

    kadence_algorithm_with_starting_and_ending

--- kadence_algorithm.js
+++ kadence_algorithm.js
@@ -1,4 +1,4 @@
-var a = [2,3,4,100,-99,-1,-25,100,15,1,2,-40,20];
+var a = [2,3,4,130,-99,-1,-52,100,15,1,2,-40,20];
 
 
 var i =0;
@@ -7,26 +7,46 @@ var length = a.length;
 
 var sum = 0;
 var lastmax = 0;
+var starting =0;
+var end = 0;
+var canbestarting = 0;
+var count =0;
+
+
 
 while(i<length){
 
 	if(a[i] >= 0) {
 
 		sum = sum + a[i];
+		
+		if(count ==1) {
+			
+			canbestarting = i;
+			
+		}
 
 		if(sum  >= lastmax ) {
 
 			lastmax = sum;
-
+			start = canbestarting;
+			ending = i;
 		}
+		count = 0;
 
 	} else if(a[i]< 0 ){
 
 		sum = sum + a[i];
+		 
 		
-		if(sum < 0)
+		if(sum < 0) { 
+			
 			sum = 0;
+			count =1;
+
+		} 
 	}
+	
 	++i;
 
 	console.log('sum',sum,'lastmax',lastmax);
@@ -34,7 +54,7 @@ while(i<length){
 }
 
 
-console.log(lastmax);
+console.log(lastmax,start,ending);
 
 
 
